d84c3718a7329f4d2db5e974eb7cb819a2e96c9d
Rico Yu <ricoyu520@gmail.com>
2024-06-20 14:55:11 +0800

    JDK1.8版本最后一次提交

--- loser-tests/src/test/java/com/loserico/openai/HelloOpenAI.java
+++ loser-tests/src/test/java/com/loserico/openai/HelloOpenAI.java
@@ -8,8 +8,8 @@ public class HelloOpenAI {
     @Test
     public void test1stApi() {
         String response = HttpUtils.post("https://api.openai.com/v1/chat/completions")
-                .addHeader("Content-Type", "application/json")
-                .bearerAuth("sk-proj-s21iDcjCegLcr8Ew9F39T3BlbkFJ0Dri2mwLmwylljFM2ocU")
+                //.addHeader("Content-Type", "application/json")
+                //.bearerAuth("sk-proj-s21iDcjCegLcr8Ew9F39T3BlbkFJ0Dri2mwLmwylljFM2ocU")
                 .body("{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"system\", \"content\": \"You are a\"}]}")
                 .request();
         String.format()
